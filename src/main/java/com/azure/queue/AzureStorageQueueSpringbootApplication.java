7a3a9c6c2f707373a165997cab8d074a48b9f890
shubham-mhatre <mhatreshubham03@gmail.com>
2025-02-23 18:37:27 +0530

    project setup initial commit

---
+++ src/main/java/com/azure/queue/AzureStorageQueueSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.azure.queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureStorageQueueSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureStorageQueueSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azure/queue/AzureStorageQueueSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureStorageQueueSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
